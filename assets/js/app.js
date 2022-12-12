f44feaecd592b6053a13c56a32723d37ced07f2e
alfredo <alfredo.arredondo@hotmail.com>
2022-12-12 12:36:13 -0300

    Desafio_JS_006

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
-
 const money = document.querySelector("#money");
 const btnMoney = document.querySelector("#btnMoney");
 const cashMoney = document.querySelector("#cashMoney");
 const totalBar = document.querySelector("#totalBar");
+const chartContainer = document.querySelector("#chartContainer");
+var chart = null;
+var dataPoints = [];
+
+chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Daily Sales Data"
+	},
+	axisY: {
+		title: "Units",
+		titleFontSize: 24
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,### Units",
+		dataPoints: dataPoints
+	}]
+});
 
 const getMoney = async (money) => {
     try {
@@ -11,6 +30,14 @@ const getMoney = async (money) => {
         const arrayMoney = await response.json();
         const total = arrayMoney.serie[0].valor * cashMoney.value;
         totalBar.textContent = "El Total es: " + total;
+        console.log(arrayMoney.serie.length);
+        for (var i = 0; i < arrayMoney.serie.length; i++) {
+            dataPoints.push({
+                x: new Date(arrayMoney.serie[i].fecha),
+                y: arrayMoney.serie[i].valor
+            });
+        }
+        chart.render(); 
     } catch {
         console.log(error);
     } 
@@ -19,36 +46,3 @@ const getMoney = async (money) => {
 btnMoney.addEventListener('click', () => {
     getMoney(money.value);
 })
-
-// var chart = new CanvasJS.Chart("chartContainer", {
-//     animationEnabled: true,
-//     theme: "light2", // "light1", "light2", "dark1", "dark2"
-//     title: {
-//         text: "Usuarios Registrados en los meses Junio y Julio",
-//     },
-//     axisY: {
-//         title: "Cantidad de Usuarios",
-//     },
-//     data: [
-//         {
-//         type: "column",
-//         showInLegend: true,
-//         legendMarkerColor: "grey",
-//         dataPoints: [
-//             { y: totalMayo, label: "Mayo" },
-//             { y: totalJunio, label: "Junio" },
-//             { y: totalJulio, label: "Julio" },
-//         ],
-//         },
-//     ],
-//     options: {
-//         backgroundColor: ["rgba(21,129, 239, 1)"],
-//         labels: {
-//         font: {
-//             family: "'Quicksand', sans-serif'",
-//             size: 14,
-//         },
-//         },
-//     },
-// });
-// chart.render();
